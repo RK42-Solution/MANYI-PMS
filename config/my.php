f057470b0fc0ea544b1c9a838710f8ffe093ed8a
RaistlinTAO <DemonVK@Gmail.com>
2019-03-09 01:46:55 +1100

    Initial commit

---
+++ config/my.php
@@ -0,0 +1,13 @@
+<?php
+$config->installed       = true;
+$config->debug           = false;
+$config->requestType     = 'PATH_INFO';
+$config->timezone        = 'Asia/Shanghai';
+$config->db->host        = '127.0.0.1';
+$config->db->port        = '3306';
+$config->db->name        = 'pms';
+$config->db->user        = 'root';
+$config->db->password    = '7ujm8ik,';
+$config->db->prefix      = 'my_';
+$config->webRoot         = getWebRoot();
+$config->default->lang   = 'en';
---
+++ tmp/log/cron.20190308.log.php
@@ -0,0 +1,72 @@
+<?php
+ die();
+21:15:45 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : OK
+
+21:15:45 task 8 executed,
+command: moduleName=webhook&methodName=asyncSend.
+return : .
+output : NO WEBHOOK EXIST.
+
+21:15:45 task 9 executed,
+command: moduleName=admin&methodName=deleteLog.
+return : .
+output : 
+21:20:45 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : OK
+
+21:20:45 task 8 executed,
+command: moduleName=webhook&methodName=asyncSend.
+return : .
+output : NO WEBHOOK EXIST.
+
+21:20:45 task 9 executed,
+command: moduleName=admin&methodName=deleteLog.
+return : .
+output : 
+21:25:45 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : OK
+
+21:25:45 task 8 executed,
+command: moduleName=webhook&methodName=asyncSend.
+return : .
+output : NO WEBHOOK EXIST.
+
+21:25:45 task 9 executed,
+command: moduleName=admin&methodName=deleteLog.
+return : .
+output : 
+21:30:45 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : OK
+
+21:30:45 task 8 executed,
+command: moduleName=webhook&methodName=asyncSend.
+return : .
+output : NO WEBHOOK EXIST.
+
+21:30:45 task 9 executed,
+command: moduleName=admin&methodName=deleteLog.
+return : .
+output : 
+21:35:45 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : OK
+
+21:35:45 task 8 executed,
+command: moduleName=webhook&methodName=asyncSend.
+return : .
+output : NO WEBHOOK EXIST.
+
+21:35:45 task 9 executed,
+command: moduleName=admin&methodName=deleteLog.
+return : .
+output : 
